649e9a98a147159da18c1aacc5c47042663bc6f5
Mohamed Muflahi <muflahimohamed18@gmail.com>
2022-01-15 12:52:16 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(array,event){
+    let x = [];
+    for (let i = 0; i < array.length; i++) {
+        x.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+        
+    }
+    return x;
+}
+function countDown(x){
+    while (x > 0) {
+        console.log(x);
+        x ++;
+    }
+}
